a8e52d05c4e833143f04be47f8f52c05c86e2513
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-06-21 17:01:00 -0300

    incluindo interpolação de strings

--- 02.py
+++ 02.py
@@ -2,19 +2,20 @@ print("Jogo Python!")
 print("---------------------")
 
 numero_secreto = 43
+maximo_tentaivas = 3
+rodada = 1
 
-tentativa = input("Digite o seu número: ")
-print("Você tentou o número: ", tentativa)
+while(rodada <= maximo_tentaivas):
+    print("Tentativa {} de {},".format(rodada, maximo_tentaivas)) 
+    tentativa = input("Digite o seu número: ")
+    print("Você tentou o número: ", tentativa)
 
-if(numero_secreto==int(tentativa)):
-    print("Acertou o número")
-else:
-    if(int(tentativa) > numero_secreto):
-        print("Seu chute foi muito alto")
-    elif(int(tentativa)<numero_secreto):
-        print("O chute foi menor do que o número desejado")
-
+    if(numero_secreto == int(tentativa)):
+        print("Acertou o número")
+    else:
+        if(int(tentativa) > numero_secreto):
+            print("Seu chute foi muito alto")
+        elif(int(tentativa) < numero_secreto):
+            print("O chute foi menor do que o número desejado")
+    rodada = rodada + 1
 print("Fim de jogo")
-
-
-
