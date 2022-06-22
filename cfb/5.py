d5ce9a0e3a14b6969b0ac02451c0917535022e33
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-06-22 15:41:38 -0300

    manipulando strings com funções

---
+++ cfb/5.py
@@ -0,0 +1,15 @@
+aula="5"
+print(bool(aula))
+
+carros=["hrv","golf","argo"]
+print(carros)
+carros[0]="fusion"
+print(carros)
+carros.append("Fit")
+print(carros)
+print(str(len(carros)))
+carros.pop()
+print(carros)
+
+
+
