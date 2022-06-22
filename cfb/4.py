02e98066406b67d95bcf8e1761aea0bdf56aff8a
Afonso Gomes de Amorim <afonso.amorim2014@hotmail.com>
2022-06-22 12:42:19 -0300

    manipulando strings

--- cfb/4.py
+++ cfb/4.py
@@ -1,4 +1,7 @@
 curso="Curso de back end"
+curso2="Novo curso"
+dia=22
+cursoEData="{} e de {} no dia {}"
 
 print(curso[0:5])
 print("Tamanho: " + str(len(curso)))
@@ -6,3 +9,14 @@ print(curso.upper())
 print(curso.replace("back","Java"))
 usoSplit=curso.split(" ")
 print(usoSplit[1])
+ 
+resposta="Python" in curso #verificando se Python está em curso
+print(resposta)
+
+resposta2=curso+curso2
+print(resposta2)
+
+print(cursoEData.format(curso,curso2,dia)) #utilizando o format
+
+
+
